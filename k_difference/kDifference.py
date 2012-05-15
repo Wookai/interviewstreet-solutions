82db6ec38d0be7c3f7aa8427b58ef7fbcfb1ff5f
Vincent Etter <vincent.etter@gmail.com>
2012-05-15 15:12:33 +0200

    Added readme and comments

--- k_difference/kDifference.py
+++ k_difference/kDifference.py
@@ -1,18 +1,23 @@
 import sys
 
+# read N and K
 firstLine = sys.stdin.readline().strip().split(' ')
 N = int(firstLine[0])
 K = int(firstLine[1])
 
+# read numbers
 numbers = [int(x) for x in sys.stdin.readline().strip().split(' ')]
 
 hashMap = {}
 for n in numbers:
+    # for each number, compute its two "differences"
     hashMap[n+K] = hashMap.get(n+K, 0) + 1
     hashMap[n-K] = hashMap.get(n-K, 0) + 1
 
 count = 0
 for n in numbers:
+    # count how many numbers are the "difference" of another
     count += hashMap.get(n, 0)
 
+# each pair has been counted twice, so divide by two
 print(count/2)
