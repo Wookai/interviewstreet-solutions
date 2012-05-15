fcf59f01f144cd4f19567716db130a1f06976c71
Vincent Etter <vincent.etter@gmail.com>
2012-05-15 15:05:09 +0200

    Solved String reduction, passes all tests!

--- string_reduction/stringReduction.py
+++ string_reduction/stringReduction.py
@@ -1,32 +1,47 @@
 import sys
 
-cache = {}
+charMap = {'ab': 'c', 'ba':'c', 'bc':'a', 'cb':'a', 'ac':'b', 'ca':'b' }
+
+# reduce the string by replacing pair of chars starting at position pos
+def reduceString(s, pos):
+    s2 = s[:pos]
+    s2 += charMap[s[pos:pos+2]]
+    s2 += s[pos+2:]
+    return s2
 
 def reduction(s):
-    if s in cache.keys():
-        return cache[s]
-    
-    minLen = len(s)
-    for i in range(len(s) - 1):
-        if s[i] != s[i + 1]:
-            s2 = ''
-            if i > 0:
-                s2 = s[:i]
-            s2 += chr(3 * 98 - ord(s[i]) - ord(s[i+1]))
-            if i + 1 < len(s):
-                s2 += s[i+2:]
-            l = reduction(s2)
-            if l < minLen:
-                minLen = l
-                
-    cache[s] = minLen
-    
-    return minLen
+    while True:
+        # count number of each letter
+        nbA = len([x for x in s if x == 'a'])
+        nbB = len([x for x in s if x == 'b'])
+        nbC = len([x for x in s if x == 'c'])
+        
+        nbTot = len(s)
+        
+        # we cannot reduce anymore if all chars are the same
+        if nbA == nbTot or nbB == nbTot or nbC == nbTot:
+            return nbTot
+        
+        # find most frequent char
+        char = 'b'
+        if nbA >= nbB and nbA >= nbC:
+            char = 'a'
+        elif nbC >= nbA and nbC >= nbB:
+            char = 'c'
+            
+        # find first occurrence of char that has a different neighbor
+        i = 0
+        while i < len(s):
+            if s[i] == char:
+                if i > 0 and s[i - 1] != char:
+                    s = reduceString(s, i - 1)
+                    break
+                elif i < nbTot and s[i + 1] != char:
+                    s = reduceString(s, i)
+                    break
+            i += 1
 
 if __name__ == '__main__':
-    first = True
-    nbLines = 0
-    count = 0
     nbLines = int(sys.stdin.readline())
     for i in range(nbLines):
         line = sys.stdin.readline().strip()
